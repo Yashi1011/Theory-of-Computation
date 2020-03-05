1c0274ef2520dec5ce5b55d31972155954c2d660
Yashi1011 <47293943+Yashi1011@users.noreply.github.com>
2020-03-05 20:55:26 +0530

    Update nfa_to_dfa.c

--- nfa_to_dfa.c
+++ nfa_to_dfa.c
@@ -1,17 +1,13 @@
 //Converting an NFA to DFA.
 
-#include <stdio.h>
+#include<stdio.h>
 
-int main()
-{
+int nfa_state[10],nfa_istate,nfa_fstate,state_counter=0,alphabets,i,j,k;
+FILE *ptr;
+char buff[20];
+int transitions[3][2][3];
 
-	//Declaring variables and pointers.
-	char buff[20],state[10],final_state;
-	char transition[3][2];
-	int state_counter=0,i;
-	FILE *ptr;
-
-	//Reading input from the file.
+void getdata(){
 	ptr = fopen("nfa_input.txt", "r");
 
 	//Checking if the pointer is NULL or not.
@@ -26,25 +22,93 @@ int main()
 		while((c=fgetc(ptr))!='\n')
 		{
 			if(c!=' '){
-				state[state_counter]=c;
+				nfa_state[state_counter]=(int)(c)-48;
 				state_counter++;
 			}
 		}
 		c=fgetc(ptr);
 
+		//Alphebets
+		fgets(buff, 20, ptr);
+
+		c=fgetc(ptr);
+		alphabets = (int)(c)-48;
+		
+		c=fgetc(ptr);
+		
 		//Transitions.
 		fgets(buff, 20, ptr);
 
 		c=fgetc(ptr);
-		for(i=0; i<3; i++)
-		{
-			while((c=fgetc(ptr))!='\n')
-			{
-				if()
+		
+		for(i=0; i<state_counter; i++){
+			for(j=0;j<alphabets;j++){
+				for(k=0;k<state_counter;k++){
+					transitions[i][j][k]=-2;
+				}
 			}
 		}
+	
+		fgets(buff, 20, ptr);
+		
+		c=fgetc(ptr);
+		for(i=0; i<state_counter; i++){
+			j=0;
+			while(c!='\n'){
+				k=0;
+				while(c!='-' && c!='\n'){
+					if(c!=' '){
+						if(c=='#'){
+							transitions[i][j][k]=-1;
+							k++;
+						}
+						else{
+							transitions[i][j][k]=(int)(c)-48;
+							k++;
+						}
+					}
+					c=fgetc(ptr);
+				}
+				j++;
+				c=fgetc(ptr);
+			}
+		}
+		
+		c=fgetc(ptr);
+		
+		//Initial state
+		fgets(buff, 20, ptr);
+
+		c=fgetc(ptr);
+		nfa_istate=(int)(c)-48;	
+		
+		c=fgetc(ptr);
+		
+		//Final state
+		c=fgetc(ptr);
+		fgets(buff, 20, ptr);
+
+		c=fgetc(ptr);
+		nfa_fstate=(int)(c)-48;	
 
 		fclose(ptr);
 	}
+}
 
+void main(){
+	getdata();
+	for(i=0;i<state_counter;i++){
+		printf("%d\n",nfa_state[i]);
+	}
+	printf("%d\n",alphabets);
+	for(i=0; i<state_counter; i++){
+		for(j=0;j<alphabets;j++){
+			for(k=0;k<state_counter;k++){
+				printf("%d ",transitions[i][j][k]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+	printf("%d\n%d\n",nfa_istate,nfa_fstate);
 }
