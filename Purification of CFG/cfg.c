43c3a03348913c211282ef02fc5df76ecc0311d1
Yashi1011 <47293943+Yashi1011@users.noreply.github.com>
2020-04-08 19:40:14 +0530

    Update cfg.c

--- Purification of CFG/cfg.c
+++ Purification of CFG/cfg.c
@@ -8,7 +8,7 @@
 
 
 //Decalring global variables/arrays.
-int i,j,k,cnt=0;
+int cnt=0;
 char epsilon;
 
 
@@ -23,24 +23,34 @@ struct Production
 
 
 //Removal of epsilon productions.
-void removeEpsilon(char epsilon)
-{
-
-    for(i=0; i<cnt; i++)
-    {
-        for(j=0; j<pro[i].n; j++)
-        {
-            int l=0;
-            char word[10]=" ";
-            for(k=0; k<strlen(pro[i].rhs[j]); k++)
-            {
-                if(pro[i].rhs[j][k]!=epsilon)
-                {
-                    word[l]=pro[i].rhs[j][k];
-                    l++;
+void removeEpsilon(char epsilon){
+	int i,j,k,l;
+	
+    for(i=0; i<cnt; i++){
+    	
+        for(j=0; j<pro[i].n; j++){
+        	
+            int m=0;
+            char word[10]="";
+            for(k=0; k<strlen(pro[i].rhs[j]); k++){
+                if(pro[i].rhs[j][k]==epsilon){
+                	for(l=0;l<strlen(pro[i].rhs[j]);l++){
+                		if(pro[i].rhs[j][l]!=epsilon){
+                			word[m]=pro[i].rhs[j][l];
+                    		m++;
+						}
+                		
+					}
+                    
                 }
+                
             }
-            strcpy(pro[i].rhs[j],word);
+            printf("%s\n",word);
+            if(strlen(word)!=0){
+            	strcpy(pro[i].rhs[pro[i].n],word);
+            	printf("--%s\n",pro[i].rhs[pro[i].n]);
+            	pro[i].n++;
+			}
         }
     }
 
@@ -53,7 +63,7 @@ int main()
 
     //Declaring a file pointer.
     FILE *f;
-
+	int i,j,k,a,b;
     //Initialising the number in productions to 0.
     for(i=0; i<10; i++)
         pro[i].n=0;
@@ -94,9 +104,9 @@ int main()
     }
 
     //Removal of epsilon productions.
-    for(int a=0; a<cnt; a++)
+    for(a=0; a<cnt; a++)
     {
-        for(int b=0; b<pro[a].n; b++)
+        for(b=0; b<pro[a].n; b++)
         {
             if(pro[a].rhs[b][0]=='#')
             {
@@ -117,5 +127,6 @@ int main()
 
         }
     }
+    
 
 }
